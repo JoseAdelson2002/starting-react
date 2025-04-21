2a79f16cb3c3ef3c8f5549e64d73c27a23e00564
jose.adelson2002 <jose.adelson2002@academico.ufs.br>
2025-04-21 16:23:56 -0300

    otimização e correção de alguns erros

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,7 @@
 import { ChevronRightIcon, Trash2 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   const navigate = useNavigate();
 
@@ -14,26 +16,20 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button
+          <Button
             onClick={() => onTaskClick(task.id)}
             className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
           >
             {task.title}
-          </button>
-          <button
-            onClick={() => onSeeDetailsClick(task)}
-            className="bg-slate-400 text-white p-2 rounded-md"
-          >
+          </Button>
+          <Button onClick={() => onSeeDetailsClick(task)}>
             <ChevronRightIcon />
-          </button>
-          <button
-            onClick={() => onDeleteTaskClick(task.id)}
-            className="bg-slate-400 text-red-600 p-2 rounded-md"
-          >
+          </Button>
+          <Button onClick={() => onDeleteTaskClick(task.id)}>
             <Trash2 />
-          </button>
+          </Button>
         </li>
       ))}
     </ul>
