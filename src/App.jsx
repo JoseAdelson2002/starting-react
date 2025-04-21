c2c6775c518d3fac5471ac6c2cce4bcdce36934b
jose.adelson2002 <jose.adelson2002@academico.ufs.br>
2025-04-21 13:55:05 -0300

    Completa o componente de adicionar tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useState } from "react";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTaskes] = useState([
@@ -44,13 +45,24 @@ function App() {
     setTaskes(newTasks);
   }
 
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+
+    setTaskes([...tasks, newTask]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center ">
           Gerenciador de Tarefas
         </h1>
-        <AddTask />
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
